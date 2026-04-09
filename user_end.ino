d2512fb0d45e7e387ecd72784c6edde456686d6b
Mia Larkin <122827012+mialarkin14@users.noreply.github.com>
2026-04-09 00:39:23 -0400

    Change packet handling and update data types
    Serial output data formatting added. Add all definitions to .h files

--- user_end.ino
+++ user_end.ino
@@ -1,35 +1,17 @@
-#include <RadioLib.h>
 #include <Wire.h>
+#include <RadioLib.h>
 #include <XPowersLib.h>
-
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define LORA_SCK 5
-#define LORA_MISO 19
-#define LORA_MOSI 27
-#define LORA_CS 18
-#define LORA_RST 23
-#define LORA_DIO1 33
-#define LORA_BUSY 32
+#include <shared_defs.h>
 
 XPowersAXP2101 PMU;
 SX1262 radio = new Module(LORA_CS, LORA_DIO1, LORA_RST, LORA_BUSY);
-
-// constants for data sending
-#define DATA_PCK_LEN    32
-#define HEADER_LEN      2
-#define DATA_BYTES      (DATA_PCK_LEN - HEADER_LEN)     // sender_id & abp
-#define ACK_LEN         3
-
-// constants for data receiving
-#define NUM_CARS        3
 
 // array index is the sender_id
 static int abp_array[NUM_CARS];
 
 
 // send an acknowledgement
-static void send_ack(uint8_t sender_id, uint8_t abp, uint8_t status) {
+static void send_ack(uint16_t sender_id, uint16_t abp, uint16_t status) {
     // create a 3 byte acknowledgement and send
     uint8_t ack[ACK_LEN];
     ack[0] = (uint8_t)(sender_id & 0xFF);
@@ -76,8 +58,8 @@ void setup() {
 }
 
 void loop() {
-    uint8_t pck[DATA_PCK_LEN];
-    int16_t st = radio.receive(pck, DATA_PCK_LEN);
+    uint16_t pck[PCK_LEN];
+    int16_t st = radio.receive((uint8_t*)pck, PCK_LEN * sizeof(uint16_t));
     
     // make sure receive is properly receieved
     if (st != RADIOLIB_ERR_NONE) {
@@ -87,23 +69,20 @@ void loop() {
     // get the packet's length
     int pck_len = radio.getPacketLength();
     // if at least one packet arrived get the sender_id and abp
-    uint8_t sender_id;
-    uint8_t abp;
-    if (pck_len >= 2) { 
+    uint16_t sender_id;
+    uint16_t abp;
+    if (pck_len >= 2 * sizeof(uint16_t)) { 
         sender_id = pck[0];
         abp = pck[1];
     }
     else { abp = 0; }
 
     // bad length
-    if (pck_len != DATA_PCK_LEN) {
+    if (pck_len != (PCK_LEN * sizeof(uint16_t))) {
         Serial.printf("Bad length=%d -> ACK(BAD_LEN) ABP=%u\n", pck_len, abp);
         send_ack(sender_id, abp, 2);
         return;
     }
-
-    // get the actual 30 byte data
-    const uint8_t* data = pck + 2;
 
     // duplicate
     if (abp_array[sender_id] == (int)abp) {
@@ -121,8 +100,27 @@ void loop() {
     // otherwise uncomment below
     // send_ack(sender_id, abp, 0);
 
-    // verify output by printing to serial monitor
-    for (int i=0; i < DATA_PCK_LEN; i++) {
-      printf("Car_ID: %d    Packet[%d]: 0x%x\n", sender_id, i, pck[i]);
+    // headers for the type of data
+    char *headers[17] = { "Time", "BMS Disch Enable",
+    "Pack Voltage", "Pack Current",
+    "Pack Temp", "State of Charge",
+    "Min Cell Voltage", "BMS LV input",
+    "Torque Feedback", "RPM",
+    "Flux Feedback", "InlineAcc",
+    "LateralAcc", "VerticalAcc",
+    "RollRate", "PitchRate", "YawRate"};
+
+    // print to serial monitor 
+    /* Outputs something similar to the following
+        Car_ID: 1 | ABP: 0
+            Time                : 0x00A3
+            BMS Disch Enable    : 0x0001
+            Pack Voltage        : 0x10B2
+            ..
+    */
+    Serial.printf("\nCar_ID: %u | ABP: %u\n", sender_id, abp);
+
+    for (int i = 2; i < PCK_LEN; i++) {
+        Serial.printf("  %-20s: %04X\n", headers[i-2], pck[i]);
     }
 }
